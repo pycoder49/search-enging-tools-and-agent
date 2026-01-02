0d48b33d5208f26b26e3e46aa692ca55c7b162b8
pycoder49 <aryan-a@outlook.com>
2026-01-01 18:39:52 -0800

    finished

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ import streamlit as st
 arxiv_wrapper = ArxivAPIWrapper(
     top_k_results=2, doc_content_chars_max=1000,
 )
-arxiv_tool = ArxivQueryRun(arxiv_wrapper)
+arxiv_tool = ArxivQueryRun(api_wrapper=arxiv_wrapper)
 
 wiki_wrapper = WikipediaAPIWrapper(
     top_k_results=2, doc_content_chars_max=1000,
 )
-wiki_tool = WikipediaQueryRun(wiki_wrapper)
+wiki_tool = WikipediaQueryRun(api_wrapper=wiki_wrapper)
 
 ddg_search = DuckDuckGoSearchRun(name="DuckDuckGo Search")
 
@@ -39,4 +39,50 @@ if "messages" not in st.session_state:
 
 for message in st.session_state.messages:
     st.chat_message(message["role"]).write(message["content"])
+
+if prompt := st.chat_input(placeholder="What is machine learning?"):
+    st.session_state.messages.append(
+        {
+            "role": "user",
+            "content": prompt,
+        }
+    )
+    st.chat_message("user").write(prompt)
+
+    llm = ChatGroq(
+        model="llama-3.1-8b-instant",
+        api_key=groq_api_key,
+        streaming=True,
+    )
+
+    tools = [arxiv_tool, wiki_tool, ddg_search]
+
+    search_agent = initialize_agent(
+        tools=tools,
+        llm=llm,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        handling_parsing_errors=True,
+    )
+
+    with st.chat_message("assistant"):
+        callback = StreamlitCallbackHandler(
+            st.container(),
+            expand_new_thoughts=True,
+        )
+
+        response = search_agent.run(
+            st.session_state.messages,
+            callbacks=[
+                callback,
+            ]
+        )
+
+        st.session_state.messages.append(
+            {
+                "role": "assistant",
+                "content": response,
+            }
+        )
+        st.write(response)
+
 
